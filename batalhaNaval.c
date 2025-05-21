4c8d2f56021d1a25a4cebab77a9d87520716a977
uimcvs <uimcvs@gmail.com>
2025-05-21 18:53:16 +0000

    Nível NOVATO

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -9,6 +9,26 @@ int main() {
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    int tabuleiro[10][10];
+    int navio1[3] = {3, 3, 3};
+    int navio2[3] = {3, 3, 3};
+    printf("teste\n");
+    for(int i = 0; i < 10; i++) {
+        for(int j = 0; j < 10; j++) {
+            if(i == 1 && j > 3 && j < 7) tabuleiro[i][j] = navio1[i-4];
+            else if(j == 8 && i > 5 && i < 9) tabuleiro[i][j] = navio2[j-6];
+            else tabuleiro[i][j] = 0;
+        }
+    }
+    printf("     A  B  C  D  E  F  G  H  I  J \n");
+    for(int i = 0; i < 10; i++) {
+        printf("%2d  ", i + 1);
+        for(int j = 0; j < 10; j++) {
+        //    printf(" t ");
+            printf(" %d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
@@ -35,6 +55,7 @@ int main() {
     // 0 0 1 0 0
     // 1 1 1 1 1
     // 0 0 1 0 0
+    getchar();
 
     return 0;
 }
